31ecd66899d075330c3227d4db84634291387699
cyyuenangie <cyyuen.angie@gmail.com>
2015-12-09 22:03:28 +0800

    Merge branch 'master' of github.com:ewenharrison/test
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- DigitRecognizer.R
+++ DigitRecognizer.R
@@ -1,23 +1,29 @@
-# Creates a simple random forest benchmark
-install.packages("randomForest")
-install.packages("readr")
+# Kaggle - Digital Recognizer
+
 library(randomForest)
 library(readr)
 
-set.seed(0)
-
-numTrain <- 10000
-numTrees <- 25
 
 train <- read_csv("~/GIT/DR/train.csv")
 test <- read_csv("~/GIT/DR/test.csv")
 
-rows <- sample(1:nrow(train), numTrain)
-labels <- as.factor(train[rows,1])
-train <- train[rows,-1]
+set.seed(0)
 
-rf <- randomForest(train, labels, xtest=test, ntree=numTrees)
-predictions <- data.frame(ImageId=1:nrow(test), Label=levels(labels)[rf$test$predicted])
+numTrain <- 10000
+numTest <- 5000
+numTrees <- 25
+
+rowtrain <- sample(1:nrow(train), numTrain)
+rowtest <- sample(1:nrow(train), numTest)
+
+train_y<- as.factor(train[rowtrain,1])
+train_x <- train[rowtrain,-1]
+
+test_y <- as.factor(train[rowtest,1])
+test_x <- train[rowtest,-1]
+
+rf <- randomForest(train_x,train_y , xtest=test_x, ntree=numTrees)
+predictions <- data.frame(ImageId=1:nrow(test_x), Label=levels(labels)[rf$test$predicted])
 head(predictions)
 
 write_csv(predictions, "rf_benchmark.csv") 
